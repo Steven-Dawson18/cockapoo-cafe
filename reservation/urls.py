6357dde6d11729b1bee86e18a7c3692d592f1d4b
Steve <la14b@yahoo.co.uk>
2021-12-17 17:05:55 +0000

    Add Update and delete reservation views, templates and urls

--- reservation/urls.py
+++ reservation/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.ReservationListView.as_view(), name="reservation"),
     path('create_reservation/', views.ReservationCreateView.as_view(), name="create_reservation"),
+    path('update_reservation/<pk>/', views.ReservationUpdateView.as_view(), name="update_reservation"),
+    path('delete_reservation/<pk>/', views.ReservationDeleteView.as_view(), name='delete_reservation'),
 ]
--- reservation/views.py
+++ reservation/views.py
@@ -23,3 +23,19 @@ class ReservationCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+
+class ReservationUpdateView(LoginRequiredMixin, UpdateView):
+    model = Reservation
+    fields = ['first_name', 'last_name', 'email', 'phone', 'time', 'datetime', 'information']
+    template_name = 'reservation/update_reservation.html'
+    success_message = "Reservation will be updated"
+    success_url = reverse_lazy('reservation')
+
+
+class ReservationDeleteView(DeleteView):
+    model = Reservation
+    fields = ['first_name', 'last_name', 'email', 'phone', 'time', 'datetime', 'information']
+    template_name = 'reservation/delete_reservation.html'
+    success_message = "Reservation will be deleted"
+    success_url = reverse_lazy('reservation')
